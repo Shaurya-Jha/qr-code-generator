58c2d3756c0240321b8ec9b749e8c4c0cafe0d1d
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-05 15:32:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   // state for getting url value from the input field
   const [url, setNewUrl] = useState("");
 
+  const defaultURL = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiBmMu475OEAxWiTGwGHbPPBOMQ78AJegQIGxAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
+
   // generate QR code event
   const generateQRCode = () => {
   //   console.log("btn clicked");
@@ -26,7 +28,14 @@ function App() {
       height: 250,
       type: "svg",
       data: url,
-
+      shape: "square",
+      image: "https://images.unsplash.com/photo-1683009427590-dd987135e66c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
+      dotsOptions:{
+        color: "#FF0000"
+      },
+      backgroundOptions:{
+        color: "#00A36C"
+      }
     })
 
     qrCode.append(document.getElementById("canvas"))
